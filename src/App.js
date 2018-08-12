f529f7201645f19be1c9dac529c3868e5f48d300
Tony Elmquist <tonyelmquist@24nettbutikk.no>
2018-08-12 13:04:21 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SimpleMap from './SimpleMap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+     <SimpleMap />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/SimpleMap.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+
+class SimpleMap extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    }
+  }
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <LeafletMap center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+
+export default SimpleMap;
