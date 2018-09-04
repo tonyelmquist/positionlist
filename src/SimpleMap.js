aea03e2b3a893c6529e5992e71b4b03e0569cf4a
Tony Elmquist <tonyelmquist@24nettbutikk.no>
2018-09-04 18:46:32 +0200

    with a rudimentary table

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,75 +1,139 @@
-import React from 'react';
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
-import axios from 'axios'
+import React from "react";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+import axios from "axios";
+
+import MUIDataTable from "mui-datatables";
 
 class SimpleMap extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       lat: 0,
       lng: 0,
       zoom: 5,
       loading: true,
       currentVessels: []
-    }
+    };
   }
 
   componentDidMount = () => {
-    this.setState({loading: true})
+    this.setState({ loading: true });
 
     axios({
-      url: 'https://api.vesseltracker.com/api/v1/vessels/userlist/latestpositions',
-      method: 'get',
+      url: "https://api.vesseltracker.com/api/v1/vessels/userlist/latestpositions",
+      method: "get",
       headers: {
-          'Authorization': '15dcbc0e-214a-49e0-8ed9-6f3e0c4a640b',
-          'Content-Type': 'application/json'
+        Authorization: "15dcbc0e-214a-49e0-8ed9-6f3e0c4a640b",
+        "Content-Type": "application/json"
       }
-   })
-   .then(response => {
-      this.parseVessels(response.data)
-   }) 
-   .catch(err => {
-      console.log(err);
-   });
-
-  }
-
-  parseVessels = (response) => {
-    this.setState({currentVessels: response.vessels})
-  }
-
-
-  plotCurrentVessels = (vessels) => {
-    const vesselPlots = vessels.map(x => ({
-      position: [
-        x.aisPosition.lat, x.aisPosition.lon
-      ], vessel: x.aisStatic.name
-    }))
-
-    const vesselPoints = vesselPlots.map((vessel, i) =>{
-      return <Marker key={i} position={vessel.position}>
-      <Popup>
-        <span>{vessel.vessel}</span>
-      </Popup>
-    </Marker>
     })
-    
-    return vesselPoints
-  }
+      .then(response => {
+        this.parseVessels(response.data);
+        this.setState({ loading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
+  };
+
+  parseVessels = response => {
+    const vesselArray = response.vessels.map(vessel => {
+      return {
+        name: vessel.aisStatic.name,
+        imo: vessel.aisStatic.imo,
+        flag: vessel.aisStatic.flag,
+        position: [vessel.aisPosition.lat, vessel.aisPosition.lon],
+        speedOverGround: vessel.aisPosition.sog,
+        heading: vessel.aisPosition.hdg,
+        destination: vessel.aisVoyage.dest,
+        ETA: vessel.aisVoyage.eta
+      };
+    });
+
+    this.setState({ currentVessels: vesselArray });
+  };
+
+  plotCurrentVessels = vessels => {
+    const vesselPoints = vessels.map((vessel, i) => {
+      return (
+        <Marker key={i} position={vessel.position}>
+          <Popup>
+            <h4>{vessel.name}</h4>
+            <p>IMO:{vessel.imo} </p>
+            <p>Destination:{vessel.destination} </p>
+            <p>Heading:{vessel.heading} </p>
+            <p>Speed:{vessel.speedOverGround} </p>
+          </Popup>
+        </Marker>
+      );
+    });
+
+    return vesselPoints;
+  };
 
   render() {
     const position = [this.state.lat, this.state.lng];
 
-    const vessels = this.state.currentVessels
+    const vessels = this.state.currentVessels;
 
+    const columns = [
+      {
+        name: "Name",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "IMO",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Destination",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      {
+        name: "Speed",
+        options: {
+          filter: true,
+          sort: true
+        }
+      },
+      
+    ];
+
+    const options = {
+      filterType: "checkbox"
+    };
+
+    const tableData = vessels.map(vessel => {
+      return [vessel.name, vessel.imo, vessel.destination, vessel.speedOverGround]
+    })
+
+    if (this.state.loading) return false;
     return (
-      <LeafletMap center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+      <div>
+        <LeafletMap center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+          {this.plotCurrentVessels(vessels)}
+        </LeafletMap>
+        <MUIDataTable
+          title={"Position List"}
+          data={tableData}
+          columns={columns}
+          options={options}
         />
-        {this.plotCurrentVessels(vessels)}
-      </LeafletMap>
+      </div>
     );
   }
 }
